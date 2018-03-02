211d1e958fc76df2ff0995041d0f2579e70a532a
gbakkk5951 <526406038@qq.com>
2018-03-02 22:54:31 +0800

    code

---
+++ KA.cpp
@@ -0,0 +1,41 @@
+class T0{}b0;
+class T1{}b1;
+template <typename t0, typename t1, typename t2, typename t3, typename t4, typename t5, typename t6, typename t7, typename t8, typename t9, typename t10, typename t11, typename t12, typename t13, typename t14, typename t15, typename t16>
+	void bomb(t0 a0, t1 a1, t2 a2, t3 a3, t4 a4, t5 a5, t6 a6, t7 a7, t8 a8, t9 a9, t10 a10, t11 a11, t12 a12, t13 a13, t14 a14, t15 a15, t16 a16) {
+		bomb(b0, a1, a2, a3, a4, a5, a6, a7, a8, a9, a10, a11, a12, a13, a14, a15, a16);
+		bomb(b1, a1, a2, a3, a4, a5, a6, a7, a8, a9, a10, a11, a12, a13, a14, a15, a16);
+		bomb(a0, b0, a2, a3, a4, a5, a6, a7, a8, a9, a10, a11, a12, a13, a14, a15, a16);
+		bomb(a0, b1, a2, a3, a4, a5, a6, a7, a8, a9, a10, a11, a12, a13, a14, a15, a16);
+		bomb(a0, a1, b0, a3, a4, a5, a6, a7, a8, a9, a10, a11, a12, a13, a14, a15, a16);
+		bomb(a0, a1, b1, a3, a4, a5, a6, a7, a8, a9, a10, a11, a12, a13, a14, a15, a16);
+		bomb(a0, a1, a2, b0, a4, a5, a6, a7, a8, a9, a10, a11, a12, a13, a14, a15, a16);
+		bomb(a0, a1, a2, b1, a4, a5, a6, a7, a8, a9, a10, a11, a12, a13, a14, a15, a16);
+		bomb(a0, a1, a2, a3, b0, a5, a6, a7, a8, a9, a10, a11, a12, a13, a14, a15, a16);
+		bomb(a0, a1, a2, a3, b1, a5, a6, a7, a8, a9, a10, a11, a12, a13, a14, a15, a16);
+		bomb(a0, a1, a2, a3, a4, b0, a6, a7, a8, a9, a10, a11, a12, a13, a14, a15, a16);
+		bomb(a0, a1, a2, a3, a4, b1, a6, a7, a8, a9, a10, a11, a12, a13, a14, a15, a16);
+		bomb(a0, a1, a2, a3, a4, a5, b0, a7, a8, a9, a10, a11, a12, a13, a14, a15, a16);
+		bomb(a0, a1, a2, a3, a4, a5, b1, a7, a8, a9, a10, a11, a12, a13, a14, a15, a16);
+		bomb(a0, a1, a2, a3, a4, a5, a6, b0, a8, a9, a10, a11, a12, a13, a14, a15, a16);
+		bomb(a0, a1, a2, a3, a4, a5, a6, b1, a8, a9, a10, a11, a12, a13, a14, a15, a16);
+		bomb(a0, a1, a2, a3, a4, a5, a6, a7, b0, a9, a10, a11, a12, a13, a14, a15, a16);
+		bomb(a0, a1, a2, a3, a4, a5, a6, a7, b1, a9, a10, a11, a12, a13, a14, a15, a16);
+		bomb(a0, a1, a2, a3, a4, a5, a6, a7, a8, b0, a10, a11, a12, a13, a14, a15, a16);
+		bomb(a0, a1, a2, a3, a4, a5, a6, a7, a8, b1, a10, a11, a12, a13, a14, a15, a16);
+		bomb(a0, a1, a2, a3, a4, a5, a6, a7, a8, a9, b0, a11, a12, a13, a14, a15, a16);
+		bomb(a0, a1, a2, a3, a4, a5, a6, a7, a8, a9, b1, a11, a12, a13, a14, a15, a16);
+		bomb(a0, a1, a2, a3, a4, a5, a6, a7, a8, a9, a10, b0, a12, a13, a14, a15, a16);
+		bomb(a0, a1, a2, a3, a4, a5, a6, a7, a8, a9, a10, b1, a12, a13, a14, a15, a16);
+		bomb(a0, a1, a2, a3, a4, a5, a6, a7, a8, a9, a10, a11, b0, a13, a14, a15, a16);
+		bomb(a0, a1, a2, a3, a4, a5, a6, a7, a8, a9, a10, a11, b1, a13, a14, a15, a16);
+		bomb(a0, a1, a2, a3, a4, a5, a6, a7, a8, a9, a10, a11, a12, b0, a14, a15, a16);
+		bomb(a0, a1, a2, a3, a4, a5, a6, a7, a8, a9, a10, a11, a12, b1, a14, a15, a16);
+		bomb(a0, a1, a2, a3, a4, a5, a6, a7, a8, a9, a10, a11, a12, a13, b0, a15, a16);
+		bomb(a0, a1, a2, a3, a4, a5, a6, a7, a8, a9, a10, a11, a12, a13, b1, a15, a16);
+		bomb(a0, a1, a2, a3, a4, a5, a6, a7, a8, a9, a10, a11, a12, a13, a14, b0, a16);
+		bomb(a0, a1, a2, a3, a4, a5, a6, a7, a8, a9, a10, a11, a12, a13, a14, b1, a16);
+		bomb(a0, a1, a2, a3, a4, a5, a6, a7, a8, a9, a10, a11, a12, a13, a14, a15, b0);
+		bomb(a0, a1, a2, a3, a4, a5, a6, a7, a8, a9, a10, a11, a12, a13, a14, a15, b1);
+}int main() {
+	bomb(b0, b0, b0, b0, b0, b0, b0, b0, b0, b0, b0, b0, b0, b0, b0, b0, b0);
+}
---
+++ 卡评测_gen.cpp
@@ -0,0 +1,55 @@
+#include <cstdio>
+// 效果M^N, 代码复杂度 M*(N^2)
+const int N = 17, M = 2;
+int main() {
+	int i, j, k;
+	
+	freopen("KA.cpp", "w", stdout);
+	
+	for (i = 0; i < M; i++) {
+		printf("class T%d{}b%d;\n", i, i);
+	}
+	
+	printf("template <");
+	for (int i = 0; i < N; i++) {
+		if (i) {
+			printf(", ");
+		}
+		printf("typename t%d", i);
+	}
+	printf(">\n	void bomb(");
+	for (i = 0; i < N; i++) {
+		if (i) {
+			printf(", ");
+		}
+		printf("t%d a%d", i, i);
+	}
+	printf(") {\n");
+	for (i = 0; i < N; i++) {
+		for (j = 0; j < M; j++) {
+			printf("		bomb(");
+			for (k = 0; k < N; k++) {
+				if (k) {
+					printf(", ");
+				}
+				if (k == i) {
+					printf("b%d", j);
+				} else {
+					printf("a%d", k);
+				}
+				
+			}
+			printf(");\n");
+		}
+	}
+	printf("}");
+	
+	printf("int main() {\n	bomb(");
+	for (i = 0; i < N; i++) {
+		if (i) {
+			printf(", ");
+		}
+		printf("b0");
+	}
+	printf(");\n}");
+}
