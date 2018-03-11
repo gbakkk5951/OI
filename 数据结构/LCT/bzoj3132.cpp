cdf873d7bc733964c4de2095b0ea402590a3be9a
gbakkk5951 <526406038@qq.com>
2018-03-12 07:28:53 +0800

    .

---
+++ 数据结构/LCT/bzoj3132.cpp
@@ -0,0 +1,15 @@
+using namespace std;
+int main() {}
+#include <cstdio>
+#include <cctype>
+#include <cstring>
+#include <cstdlib>
+#include <algorithm>
+namespace OI {
+const int MXN = 80010;
+struct Node {
+	Node *s[2], *f;
+	int size;
+}pool[80];
+
+}
