f921358e8b273633c1bdce991d4a5745345439eb
gbakkk5951 <526406038@qq.com>
2018-03-23 12:35:03 +0800

    写题

--- FFT/CC分块FFT.cpp
+++ FFT/CC分块FFT.cpp
@@ -6,6 +6,7 @@ int main() {}
 #include <cstdio>
 #include <cmath>
 namespace OI {
+const lf pi = acos(-1.0);
 int sq = 173;
 const int SQN = 180, MXN = 100005, MX = 30005;
 const lld ans;
@@ -15,15 +16,50 @@ struct com {
 };
 
 namespace FFTSpace {
-	
-	void FFT(com arr[], int len) {
-		static com w[65537], w, x, y;
-		int i, j, k;
-		com *a, *b;
-		for (int I = 1; 1 << I <= len; I++) {
-			
+void mov_ele(com arr[], int len) {
+	int i, j = 0, k;
+	for (i = 1; i < len; i++) {
+		for (k = len >> 1; k & j; k >>= 1) {
+			j ^= k;
+		}
+		j |= k;
+		if (i < j) {
+			swap(arr[i], arr[j]);
 		}
 	}
+}
+void FFT(com arr[], int len, int pow) {
+	static com w[32769], wn, x, y;
+	int i, j, k, half;
+	com *a, *b;
+	w[0].real = 1, w[0].imag = 0;
+	mov_ele(arr, len);
+	for (int I = 1; 1 << I <= len; I++) {
+		half = 1 << I - 1;
+		wn.real = cos(2.0 * pi / (1 << I));
+		wn.imag = pow * sin(2.0 * pi / (1 << I));
+		for (int i = (1 << I - 1) - 2; i; i -= 2) {
+			w[i] = w[i >> 1];
+		}
+		for (int i = 1; i < 1 << I - 1; i += 2) {
+			w[i] = w[i - 1] * wn;
+		}
+		for (int i = 0; i < len; i += half << 1) {
+			a = arr + i; b = arr + i + half;
+			for (int j = 0; j < half; j++) {
+				x = a[j];
+				y = b[j] * wn[j];
+				a[i] = x + y;
+				b[i] = x - y;
+			}
+		}
+	}
+	if (pow == -1) {
+		for (int i = 0; i < len; i++) {
+			w[i] /= len;
+		}
+	}
+}
 }
 
 struct _Main {
@@ -34,6 +70,7 @@ struct _Main {
 	int n;
 	com c1[65537], c2[65537];
 	void maincalc() {
+		
 		for (int i = 2; i < id[n]; i++) {
 			memset(c1, 0, 2 * MX * sizeof(com));
 			memset(c2, 0, 2 * MX * sizeof(com));
@@ -44,14 +81,14 @@ struct _Main {
 				c2[arr[j]].r += 1;
 			}
 			using FFTSpace::FFT;
-			FFT(c1, MX, 1);
-			FFT(c2, MX, 1);
+			FFT(c1, 65536, 1);
+			FFT(c2, 65536, 1);
 			for (int j = 0; j < MX; j++) {
 				c1[j] = c1[j] * c2[j];
 			}
 			FFT(c1, MX, -1);
 			for (int j = st[i]; j <= ed[i]; j++) {
-				ans += round(c1[arr[j] * 2].r);
+				ans += round(c1[arr[j] * 2].real);
 			}
 		}
 	}
@@ -94,10 +131,7 @@ struct _Main {
 			}
 		}
 	}
-	
-	void maincalc() {
-	
-	}
+
 	_Main() {
 		read(n);
 		if (int i = 1; i <= n; i++) {
