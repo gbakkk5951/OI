2ee31667246d0b2ead4212d1aa256a737fca39d1
gbakkk5951 <526406038@qq.com>
2018-05-18 14:45:29 +0800

    update .gitignore

--- __history/17.5/cdvs2100/test.bat
+++
@@ -1,6 +0,0 @@
-
-:loop
-	cdvs2100_Make.exe
-	cdvs2100_New.exe
-	echo a
-goto loop
--- __history/18.2/网络流/未命名1.cpp
+++
@@ -1,7 +0,0 @@
-#include <cstdio>
-const long long MOD = 507;
-int main() {
-    long long a = 9223372036854775808LLU;
-    printf("%lld\n", a);
-    printf("%lld", (a % MOD + MOD) % MOD);
-}
