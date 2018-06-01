a14b351ef7674a98b4d69329c45f35189160e136
gbakkk5951 <526406038@qq.com>
2018-06-01 17:52:43 +0800

    写题

---
+++ 计算几何/辛普森积分/bzoj1052[Noi2005]月下柠檬树/std.cpp
@@ -0,0 +1,14 @@
+using namespace std;
+int main() {}
+#include <cstdio>
+#include <cctype>
+#include <cstring>
+#include <cstdlib>
+#include <algorithm>
+#include <cmath>
+namespace OI {
+typedef double lf;
+const lf EPS = 1e-7;
+	
+	
+}
